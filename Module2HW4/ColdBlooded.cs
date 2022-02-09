ecc05cd1748f1192a2295cb28a3695708f0b6bd0
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-02-09 19:43:10 +0200

    delete something

--- Module2HW4/ColdBlooded.cs
+++ Module2HW4/ColdBlooded.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Module2HW4
+﻿namespace Module2HW4
 {
     public class ColdBlooded : Reptile
     {
